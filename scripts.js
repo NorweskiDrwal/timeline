1f0fce4a26cff3f2f40cd5d45ad4198e9a29ed1e
NorweskiDrwal <30369902+NorweskiDrwal@users.noreply.github.com>
2017-11-24 16:50:54 +0100

    scripts - changes
    minor tweaks

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-    var section = document.querySelector('section');
+    var eventList = document.querySelector('#eventList');
     var requestURL = 'events.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
@@ -14,21 +14,14 @@
     function showHeroes(jsonObj) {
       var heroes = jsonObj['members'];
       for(var i = 0; i < heroes.length; i++) {
-        var myArticle = document.createElement('article');
+        var eventListItem = document.createElement('li');
         var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
         myH2.textContent = heroes[i].data;
-        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
         myPara2.textContent = 'Wydarzenie: ' + heroes[i].nazwa;
 
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
-        section.appendChild(myArticle);
+        eventListItem.appendChild(myH2);
+        eventListItem.appendChild(myPara2);
+        eventList.appendChild(eventListItem);
       }
     }
